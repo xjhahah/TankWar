71f6ea1f36f28eec3f82973841a1b16fa5403ebd
xjhahah <908234867@qq.com>
2018-11-30 20:29:32 +0800

    修正代码

---
+++ Tank_War/common.h
@@ -0,0 +1,23 @@
+#pragma once
+
+
+//己方坦克坐标
+unsigned char g_OurPosX = 7;
+unsigned char g_OurPosY = 20;
+
+unsigned char g_OurPosTempX = 7;
+unsigned char g_OurPosTempY = 20;
+
+//敌人坦克坐标
+unsigned char g_1DiPosX = 2;
+unsigned char g_1DiPosY = 2;
+
+unsigned char g_1DiPosTempX = 2;
+unsigned char g_1DiPosTempY = 2;
+
+
+unsigned char g_2DiPosX = 19;
+unsigned char g_2DiPosY = 2;
+
+unsigned char g_2DiPosTempX = 19;
+unsigned char g_2DiPosTempY = 2;
--- Tank_War/main.c
+++ Tank_War/main.c
@@ -1,85 +1,18 @@
 #include "Tank.h"
 
 
-//显示提示界面
-void StartIU()
-{
-	printf("\n\n\n\t\t\t  《 欢迎来到坦克大战 》\n\n");
-	printf("\t\t\t《 W S A D 控制坦克行进 》\n\n");
-	printf("\t\t\t    《 空格发射子弹 》\n\n");
-	printf("\t\t\t  《 按任意键开始游戏 》\n\n");
-}
-//按任意键开始游戏
-void ClearIU()
-{
-	system("cls");
-}
-
-//按任意键开始游戏
-void StartGame()
-{
-	_getch(); //不阻塞读取
-}
-void ShowBackGroud()
-{
-	int i = 0;
-	int j = 0;
-
-	char strBackTemp[1024] = { 0 };
-
-	for (i = 0; i < BACK_HIGHT; i++)
-	{
-		for (j = 0; j < BACK_WIDE; j++)
-		{
-			switch (g_arrBackGround[i][j])
-			{
-			case 0: //空格
-				///printf("  ");
-				strcat(strBackTemp, "  ");
-				break;
-			case 1: //边框
-				//printf("■");
-				strcat(strBackTemp, "■");
-				break;
-			case 2:  //己方坦克
-				//printf("★");
-				strcat(strBackTemp, "★");
-				break;
-			case 3:  //敌方坦克
-				//printf("☆");
-				strcat(strBackTemp, "☆");
-				break;
-			case 4: // 乙方boss      
-				//printf("◆");
-				strcat(strBackTemp, "◆");
-				break;
-			case 5: // 乙方boss      
-				//printf("◆");
-				strcat(strBackTemp, "●");
-				break;
-			}
-		}
-		//换行
-		//printf("\n");
-		strcat(strBackTemp, "\n");
-	}
-
-	printf("%s", strBackTemp);
-}
-
-
 int main()
 {
 	srand((unsigned int)time(NULL));
 	//显示提示界面
-	StartIU();
+	//StartIU();
 	//按任意键开始游戏
-	StartGame();
+	//StartGame();
 	//按任意键开始游戏
-	ClearIU();
+	//ClearIU();
 
 	//显示边框
-	ShowBackGroud();
+	//ShowBackGroud();
 
 	int sudu = 0;
 
